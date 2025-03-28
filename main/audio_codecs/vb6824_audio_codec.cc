bd0356fdff1883ed0ec49ac064c2e92ce9dfb6e2
HoGC <1050517465@qq.com>
2025-03-28 09:46:34 +0800

    vb6824 ready改为20ms

--- main/audio_codecs/vb6824_audio_codec.cc
+++ main/audio_codecs/vb6824_audio_codec.cc
@@ -80,7 +80,7 @@ void VbAduioCodec::Start() {
         esp_timer_create(&timer_args, &power_save_timer_);
     }
     if(power_save_timer_){
-        esp_timer_start_periodic(power_save_timer_, 5*1000);
+        esp_timer_start_periodic(power_save_timer_, 20*1000);
     }
 }
 
