d5294609c375e7a435406c1bb5d63e56e4a0358a
HoGC <1050517465@qq.com>
2025-06-05 11:17:21 +0800

    修复ReleaseDecoder没有定义问题

--- main/application.h
+++ main/application.h
@@ -88,8 +88,8 @@ public:
     }
     void StartCheckNewVersionForCustom();
 #endif
-#if defined(CONFIG_VB6824_OTA_SUPPORT) && CONFIG_VB6824_OTA_SUPPORT == 1
     void ReleaseDecoder();
+#if defined(CONFIG_VB6824_OTA_SUPPORT) && CONFIG_VB6824_OTA_SUPPORT == 1
     void ShowOtaInfo(const std::string& code, const std::string& ip="");
 #endif
 
